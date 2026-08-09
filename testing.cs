bf66c9f1e5ee6691dbe6f94b1825e069d892e5de
Karuu <carl.mendiola@umak.edu.ph>
2026-08-09 21:14:47 +0800

    testing.cs is only for testing the block of code before putting in the main file.

--- testing.cs
+++ testing.cs
@@ -1,7 +1,1 @@
- Console.WriteLine("Checking if a Number is Positive, Negative, or Zero (using the ternary operator)");
-
-         Console.Write("Enter a Number: ");
-         int ternaryNum = int.Parse(Console.ReadLine());
-
-         string result = (ternaryNum > 0) ? "Postive" : (ternaryNum < 0) ? "Negative" : "Zero";
-         Console.WriteLine($"The number {ternaryNum} is {result}");
+//this file is for testing the block of code only, it is not part of the main program
