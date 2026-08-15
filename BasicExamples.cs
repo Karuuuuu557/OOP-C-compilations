77fde51e55be8be9ce4c2d4b2b24d9c10850ecb7
Karuu <carl.mendiola@umak.edu.ph>
2026-08-15 21:54:24 +0800

    new prog testing: arrays

--- BasicExamples.cs
+++ BasicExamples.cs
@@ -193,5 +193,30 @@ Expected Output:
     Console.WriteLine($"{num1} / {num2} is: {dev}");
     Console.WriteLine($"{num1} % {num2} is: {mod}");
 
+//ML 2 : Array
+    Console.WriteLine("Enter number of elements: ");
+    int size = int.Parse(Console.ReadLine());
+
+    int [] myArray = new int[size];
+
+    Console.WriteLine("Enter the elements of the array:");
+    for (int i = 0; i < size; i++);
+        {
+            Console.WriteLine($"Element {i + 1}: ");
+            myArray[i] = int.Parse(Console.ReadLine());
+        }
+/*
+1.	Display the Elements
+2.	Access Array Elements (First and Last)
+3.	Add an Element to the Array
+4.	Modify an Array Element 
+5.	Remove an Element from the Array
+6.	Calculate and Print the Sum of Array Elements
+7.	Reverse the Array
+*/
+
+// 1. Display the Elements
+    Console.WriteLine("\nOriginal Array");
+    PrintArray(myArray);
         }
     }
--- testing.cs
+++ testing.cs
@@ -3,11 +3,42 @@ using System;
 
 class Program
 {
+    /*
+INSTRUCTIONS:
+1.	Display the Elements
+2.	Access Array Elements (First and Last)
+3.	Add an Element to the Array
+4.	Modify an Array Element 
+5.	Remove an Element from the Array
+6.	Calculate and Print the Sum of Array Elements
+7.	Reverse the Array
+*/
     static void Main()
     {
-        Console.WriteLine("Enter a Number: ");
-        int num = int.Parse(Console.ReadLine());
+        //ML 2 : Array
+    Console.WriteLine("Enter number of elements: ");
+    int size = int.Parse(Console.ReadLine());
 
-        Console.WriteLine($"The number you inputted is: {num}");
+    int [] myArray = new int[size];
+
+    Console.WriteLine("Enter the elements of the array:");
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Element {i + 1}: ");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+
+
+// 1. Display the Elements
+    Console.WriteLine("\nOriginal Array");
+    PrintArray(myArray);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int element in arr)
+        {
+            Console.WriteLine(element);
+        }
     }
 }
